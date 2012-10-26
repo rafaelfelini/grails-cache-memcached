a31df20755e5d77f59caa132980244bbc7ee190a
Rafael Felini <rafael.felini@gmail.com>
2012-10-26 10:58:02 -0200

    Atualizacao do BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,22 +3,16 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+
     inherits("global") {
         // uncomment to disable ehcache
 		excludes 'ehcache'
     }
+	
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
+    
+	repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
         mavenRepo 'http://files.couchbase.com/maven2/' //spymemcached
     }
     dependencies {
